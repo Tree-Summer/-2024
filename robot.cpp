7d195819e39752a8d47890b0ed18efa8a10ae0ed
mycroft <932111799@qq.com>
2024-03-08 22:53:10 +0800

    finish_robot

--- robot.cpp
+++ robot.cpp
@@ -1,19 +1,26 @@
 #include "robot.h"
 
 
-
+bool beside(Step* a, Step* b){ //判断相邻
+    if(abs(a->x - b->x) + abs(a->y - b->y) == 1)
+        return true;
+    else
+        return false;
+}
 
 string Robot::find_path(Map *map) {
     queue<Step*> queue1;
     vector<Step*> vector1;
     string path;
-    int rec[200][200];
+    int rec[200][200];  //记录足迹
     Point* u = new Point(this->x, this->y, 1);
     Step* step = new Step(u->x, u->y, 0);
     rec[this->x][this->y] = 1;
     queue1.push(step);
     vector1.push_back(step);
     int distance = 0;
+
+    //寻找第一个货物
     while(!queue1.empty()){
         Step* s = queue1.front();
         queue1.pop();
@@ -21,13 +28,13 @@ string Robot::find_path(Map *map) {
         int x_pos = s->x + 1;
         int y_pos = s->y;
         if(x_pos >= 0 && x_pos < 200 && y_pos >= 0 && y_pos < 200){
-            if(map->cnt[x_pos][y_pos]->type == 1 && rec[x_pos][y_pos] == 0){
+            if(map->cnt[x_pos][y_pos]->type == 1 && rec[x_pos][y_pos] == 0){   //type==1 陆地
                 Step* step1 = new Step(x_pos, y_pos, s->dist+1);
                 queue1.push(step1);
                 vector1.push_back(step1);
                 rec[x_pos][y_pos] = 1;
             }
-            else if(map->cnt[x_pos][y_pos]->type == 3 && rec[x_pos][y_pos] == 0){
+            else if(map->cnt[x_pos][y_pos]->type == 3 && rec[x_pos][y_pos] == 0){ //type==3 货物
                 Step* end_step = new Step(x_pos, y_pos, s->dist + 1);
                 vector1.push_back(end_step);
                 queue1.push(end_step);
@@ -88,5 +95,26 @@ string Robot::find_path(Map *map) {
         //
     }
     Step* destination = vector1[vector1.size()-1];
-    
+    int step_size = destination->dist;
+    for(int i = 1; i <= step_size; i++) {              //获取路径
+        for(int j = 0;j < vector1.size(); j++){
+            if(vector1[j]->dist == destination->dist - 1 && beside(vector1[j], destination)){
+                if(vector1[j]->x + 1 == destination->x)
+                    path.push_back('0');
+                else if (vector1[j]->x - 1 == destination->x)
+                    path.push_back('1');
+                else if(vector1[j]->y + 1 == destination->y)
+                    path.push_back('3');
+                else if(vector1[j]->y - 1 == destination->y)
+                    path.push_back('2');
+                destination = vector1[j];
+                break;
+            }
+        }
+    }
+    string ans;
+    for(int i = path.size()-1; i>=0 ; i--){
+        ans.push_back(path[i]);
+    }
+    return ans;
 }
