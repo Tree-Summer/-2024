148a541a37376e0c47290362ed4587914dfcd013
assassain777 <1123496708@qq.com>
2024-03-10 00:36:05 +0800

    change

--- ship.h
+++ ship.h
@@ -1,10 +1,9 @@
-#include<iostream>
 #include"bowei.h"
 using namespace std;
 
 int boat_capacity;
 
-class boat
+class Boat
 {
     public:
     int id;//beat's id
@@ -22,32 +21,34 @@ class boat
     void statuschange(int nowtime);
     void backtoberth(int nowtime);
     
-}boat[5];
+};
+
+Boat boat[5];
 
 // change the berth
-void boat::ship(int berthid,int nowtime){
+void Boat::ship(int berthid,int nowtime){
     status =0, pos = berthid;
     time = nowtime + 1000;
     printf("ship %d %d",id,pos);
 }
 
 // go to virtual point
-void boat::go(int nowtime){
+void Boat::go(int nowtime){
     if(num==boat_capacity){
-        time = nowtime + berth[pos].transport_time;
+        time = nowtime +berth[pos].transport_time;
         status =0,pos =-1;
    }
 }
 
 // from virtual point back to berth(stable)
-void boat::backtoberth(int nowtime){
+void Boat::backtoberth(int nowtime){
     num = 0;
-    time = nowtime + berth[boweiid].transport_time;
+    time = nowtime+ berth[boweiid].transport_time;
     status =0,pos =boweiid;
 }
 
 //if the boat succeed to reach the goal
-void boat::statuschange(int nowtime){
+void Boat::statuschange(int nowtime){
     //if(time==nowtime) status = 1;
     if(pos==-1&&time==nowtime) backtoberth(nowtime); 
     /*if(pos==-1&&time==nowtime) status = 1;
