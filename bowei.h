953a52d272ba2e2534afcaea78b0b9bc91442b59
assassain777 <1123496708@qq.com>
2024-03-09 23:34:55 +0800

    CHUAN

--- bowei.h
+++ bowei.h
@@ -5,13 +5,22 @@ class Berth
 {
     public:
     int x,y;
-    int transporttime;
-    int loadingspeed;
+    int transport_time;
+    int loading_speed;
+    int boatid;
+    
+/*
+    int boatid[11];
+    int boatnum,boatfirst,boatend;
+*/
     Berth(){}
     Berth(int x, int y, int transporttime, int loadingspeed) {
         this -> x = x;
         this -> y = y;
-        this -> transporttime = transporttime;
-        this -> loadingspeed = loadingspeed;
+        this -> transport_time = transporttime;
+        this -> loading_speed = loadingspeed;
+        this -> boatid = -1;
+        //for(int i=0;i<11;i++) this -> boatid[i] = -1;
     }
+
 }berth[20];
